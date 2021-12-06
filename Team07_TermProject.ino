8b801521b0c1f6c9e78eb7e3392cab9977b6d618
yunekorea <kooopooo@naver.com>
2021-12-06 18:50:20 +0900

    added gas and temp/humid sensor

--- Team07_TermProject.ino
+++ Team07_TermProject.ino
@@ -1,17 +1,22 @@
+#include <MQ135.h>
+#include "DHTStable.h" //DHTStable by Rob Tillaart
+
 #include <Servo.h>
 
-const int gasSensor = ;
-const int rainDropSensor = ;
-const int tempSensor  = ;
-const int humidSensor = ;
-const int IRSensor = ;
-const int servoMotor = ;
+const int gasSensor = A2;
+//const int rainDropSensor = ;
+const int tempHumidSensor  = 5;
+//const int IRSensor = ;
+//const int servoMotor = ;
 
-int gas = 0;
+float gas = 0;
 int rainDrop = 0;
-int temp = 0;
-int humid = 0;
+float temp = 0;
+float humid = 0;
 int IR = 0;
+
+MQ135 mq135Sensor(gasSensor);
+DHTStable DHT;
 
 Servo windowServo;    //Servo motor that controls the window
 
@@ -21,11 +26,11 @@ void SensorInit(void) {
   //RainDrop Sensor init
 
   //Temp Sensor init
-
+  
   //humid Sensor init
-
+  
   //IRSensor init
-  pinMode(IRSensor, INPUT);
+  //pinMode(IRSensor, INPUT);
 }
 
 void MotorInit(void) {
@@ -33,8 +38,17 @@ void MotorInit(void) {
   
 }
 
-void SensorData(int *gas, int* rain, int* temp, int* humid, int* IR) {
+void SensorData() {
   //getting data from each sensors
+
+  //DHT11 data read
+  DHT.read11(tempHumidSensor);
+  //get temp
+  temp = DHT.getTemperature();
+  //get humidity
+  humid = DHT.getHumidity();
+  //get and correct the gas data based on temperature and humidity
+  gas = mq135Sensor.getCorrectedPPM(temp, humid);
 }
 
 void OpenWindow(void) {
@@ -48,7 +62,7 @@ void CloseWindow(void) {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  windowServo.attatch(servoMotor);
+  //windowServo.attatch(servoMotor);
 
   SensorInit();
   MotorInit();
@@ -56,11 +70,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  SensorData(&gas, &rainDrop, &temp, &humid, &IR);
+  SensorData();
+  
+  Serial.println("temperature : " + String(temp));
+  Serial.println("humidity : " + String(humid));
+  Serial.println("gas : " + String(gas));
   if(rainDrop) {        //raining
     if(!IR)             //Window is open
       CloseWindow();
   } else {              //not raining
     //implement
   }
+  delay(2000);
 }
