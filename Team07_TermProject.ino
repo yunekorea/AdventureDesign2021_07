36f7f8cfbf9c2c81bbb913ccc82e360641da0180
yunekorea <kooopooo@naver.com>
2021-12-13 19:47:05 +0900

    added motor functions

--- Team07_TermProject.ino
+++ Team07_TermProject.ino
@@ -3,12 +3,24 @@
 
 #include <Servo.h>
 
+//DC Motor pin
+#define DCOPEN_EN   38
+#define DCOPEN_PWM  9
+#define DCOPEN_DIR  39
+#define DCCLOSE_EN  40
+#define DCCLOSE_PWN 10
+#define DCCLOSE_DIR 41
+
+const int switchButton = ;
+
 const int gasSensorIn = A2;
 const int gasSensorOut = A3;
 //const int rainDropSensor = ;
 const int tempHumidSensorIn = 5;
 const int tempHumidSensorOut = 6;
-//const int IRSensor = ;
+const int IRSensorOpen = 3;
+const int IRSensorClose = 4;
+
 //const int servoMotor = ;
 
 float gasIn = 0;
@@ -18,7 +30,8 @@ float tempOut = 0;
 float humidIn = 0;
 float humidOut = 0;
 int rainDrop = 0;
-int IR = 0;
+int IROpen = 0;
+int IRClose = 0;
 
 MQ135 mq135In(gasSensorIn);
 MQ135 mq135Out(gasSensorOut);
@@ -35,13 +48,23 @@ void SensorInit(void) {
   //Temp/Humid Sensor init
   
   //IRSensor init
-  //pinMode(IRSensor, INPUT);
+  pinMode(IRSensorOpen, INPUT);
+  pinMode(IRSensorClose, INPUT);
 }
 
 void MotorInit(void) {
-  //initializing servo or DC motor
-  
+  //initializing DC motor
+    pinMode(DCOPEN_EN, OUTPUT);
+  pinMode(DCOPEN_PWM, OUTPUT);
+  pinMode(DCOPEN_DIR, OUTPUT);
+  pinMode(DCCLOSE_EN, OUTPUT);
+  pinMode(DCCLOSE_PWM, OUTPUT);
+  pinMode(DCCLOSE_DIR, OUTPUT);
+  digitalWrite(DCOPEN_EN, HIGH);
+  digitalWrite(DCCLOSE_EN, HIGH);
 }
+
+
 
 void SensorData() {
   //getting data from each sensors
@@ -59,14 +82,27 @@ void SensorData() {
   //get and correct the gas data based on temperature and humidity
   gasIn = mq135In.getCorrectedPPM(tempIn, humidIn);
   gasOut = mq135Out.getCorrectedPPM(tempOut, humidOut);
+
+  //IRSensor read
+  IROpen = digitalRead(IRSensorOpen);
+  IRClose = digitalRead(IRSensorClose);
 }
 
-void OpenWindow(void) {
-  
+void openWindow(){
+  digitalWrite(DCOPEN_DIR, HIGH); // 정방향 회전
+  //int pwm_value = 255 - speed;
+  while(digitalRead(IRSensorOpen) ||
+        !digitalRead(IRSensorClose))//열려있지 않거나 닫혀있는 동안 
+    analogWrite(DCOPEN_PWM, 0);
+  analogWrite(DCOPEN_PWM, 255);
 }
 
 void CloseWindow(void) {
-  
+  digitalWrite(DCCLOSE_DIR, HIGH);
+  while(digitalRead(IRSensorClose) ||
+        !digitalRead(IRSensorOpen))//닫혀있지 않거나 열려있는 동안 
+    analogWrite(DCCLOSE_PWM,0);
+  analogWrite(DCCLOSE_PWM, 255);
 }
 
 void setup() {
