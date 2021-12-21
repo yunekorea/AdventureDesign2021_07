8170caaf472c6ba4f10ca5e4c2ac1021e7a9eeb3
arogyaGurkha <kharelarogya@hotmail.com>
2021-12-21 14:06:47 +0900

    Add additional comments for readability

--- DHT/DHT.ino
+++ DHT/DHT.ino
@@ -10,17 +10,38 @@ DHTStable DHTout; // Outside the building
 // 핀 번호 (RS, E, DB4, DB5, DB6, DB7)
 LiquidCrystal lcd(44, 45, 46, 47, 48, 49); // LCD 연결
 
+// Motors
+int Enable1 = 38; // 모터 1
+int PWM1 = 9;
+int DIR1 = 39;
+int Enable2 = 40; // 모터 2
+int PWM2 = 10;
+int DIR2 = 41;
+
+// IR Sensor
+int IROpen = 3;
+int IRClose= 4;
+
+// RGB LED
 int RGB_LED[] = {6, 7, 8};
 
+// Global variables
 float tempIn = 0, tempOut = 0;
 float humidIn = 0, humidOut = 0;
 bool isWindowOpen = false;
 
+/*
+ * Function to print logs
+ * Accepts a log_message and a value to print. Can also only accept log message.
+ */
 void Log(String log_message, String value = "") { // Function to print logs
   Serial.println(log_message + value + '\n');
 }
 
-void ht_read() { // Read humidity and temperature values
+/*
+ * Read humidity and temperature values
+ */
+void ht_read() { 
   // start reading DHT data
   DHTin.read11(tempHumidSensorIn);
   DHTout.read11(tempHumidSensorOut);
@@ -34,7 +55,11 @@ void ht_read() { // Read humidity and temperature values
   humidOut = DHTout.getHumidity();
 }
 
-int read_temperature_states(float _temperature) { // Divide temperature into 0:cold, 1:moderate, 2:hot
+/*
+ * Separate temperature into 3 separate states. This is mainly used by the RGB LED.
+ * Divide temperature into 0:cold, 1:moderate, 2:hot
+ */
+int read_temperature_states(float _temperature) { 
 
   if (_temperature < 18.00) {
     return 0;
@@ -48,10 +73,16 @@ int read_temperature_states(float _temperature) { // Divide temperature into 0:c
 
 }
 
+/*
+ * This will display the current temperature in LCD. No parameters necessary.
+ */
 void display_temperature_LCD() {
   lcd.print(tempIn);
 }
 
+/*
+ * Sets the RGB color depending on the temperature state.
+ */
 void set_RGB_color{
   int setting = read_temperature_states(tempOut);
   int colorValue = map(tempOut, 0, 18, 255, 120);
@@ -78,7 +109,10 @@ void set_RGB_color{
   }
 }
 
-bool is_humid_out() { // Check if outside is more humid than inside
+/*
+ * Checks if outside is more humid that the inside
+ */
+bool is_humid_out() { 
   bool isMoreHumidOut = true;
 
   if (humidOut < humidIn) {
@@ -88,6 +122,10 @@ bool is_humid_out() { // Check if outside is more humid than inside
   return isMoreHumidOut;
 }
 
+
+/*
+ * Opens the window
+ */
 void open_window() { // Open the window
   if (isWindowOpen == false) {
     isWindowOpen = true;
@@ -95,6 +133,9 @@ void open_window() { // Open the window
   }
 }
 
+/*
+ * Closes the window
+ */
 void close_window() { // Close the window
   if (isWindowOpen == true) {
     isWindowOpen = false;
